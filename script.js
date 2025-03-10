df92af3e26be3dcf834c2671b9ce45d802d528cc
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-10 23:46:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-	let len-1 = str.length;
+	let len = str.length-1;
 	for(let i=0 ; i<len/2 ; i++){
 		if(str[i] !== str[l-i]){
 			return false;
